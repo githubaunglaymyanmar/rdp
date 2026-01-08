cc07043296d32995751189a787b5ef37c36608a4
githubaunglaymyanmar <101466577+githubaunglaymyanmar@users.noreply.github.com>
2026-01-08 11:09:25 +0630

    Update finalrdp.sh

--- finalrdp.sh
+++ finalrdp.sh
@@ -15,7 +15,7 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
-#apt install --assume-yes xfce4 desktop-base
+
 apt update
 
 sudo debconf-set-selections <<< "keyboard-configuration keyboard-configuration/layoutcode string us"
