5638575689e08598f99a2a5ab3611574367cf7bb
githubaunglaymyanmar <101466577+githubaunglaymyanmar@users.noreply.github.com>
2026-01-08 11:13:28 +0630

    Update finalrdp.sh

--- finalrdp.sh
+++ finalrdp.sh
@@ -14,16 +14,7 @@ sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
-sudo DEBIAN_FRONTEND=noninteractive \
-
-apt update
-
-sudo debconf-set-selections <<< "keyboard-configuration keyboard-configuration/layoutcode string us"
-sudo debconf-set-selections <<< "keyboard-configuration keyboard-configuration/modelcode string pc105"
-apt update
-sudo DEBIAN_FRONTEND=noninteractive apt install --assume-yes xfce4 desktop-base
-
-
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y xfce4 desktop-base
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
